8751289c7e1f5c7b25c155f12c547bf0bd250c96
Lucas Paes Januzi <lpjanuzi@gmail.com>
2022-03-07 18:34:29 -0300

    Adicionando exercícios da prática

---
+++ fundamentos/bloco04JavascripteLogica/bloco04-01/pratica/program1.js
@@ -0,0 +1,10 @@
+// 1-
+
+const a = 10;
+const b = 20;
+
+let sum = a + b;
+let sub = a - b;
+let mul = a * b;
+let div = a / b;
+let mod = a % b;
---
+++ fundamentos/bloco04JavascripteLogica/bloco04-01/pratica/program2.js
@@ -0,0 +1,14 @@
+// 2-
+
+const a = 40;
+const b = 50;
+
+if (a > b) {
+    console.log("O maior número é " + a)
+}
+else if (a < b) {
+    console.log("O maior número é " + b)
+}
+else {
+    console.log("Os valores são iguais")
+}
