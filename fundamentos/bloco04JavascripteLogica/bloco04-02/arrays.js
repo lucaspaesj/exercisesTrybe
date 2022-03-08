828da9aad811a3f5d2c42655f5ff8bc3f05df9ed
Lucas Paes Januzi <lpjanuzi@gmail.com>
2022-03-08 15:53:01 -0300

    Adicionando conteúdo sobre for

--- fundamentos/bloco04JavascripteLogica/bloco04-02/arrays.js
+++ fundamentos/bloco04JavascripteLogica/bloco04-02/arrays.js
@@ -4,6 +4,9 @@ let pizzas = ["4 Queijos", "Frango com Catupiry", "Marguerita", "Palmito", "Choc
 
 pizzas.push("Peito de peru"); // Adicionar um elemento ao final do Array
 pizzas.unshift("Gratinada"); // Adiciona um elemento ao final do Array
+pizzas.pop(); // Remove o último elemento do Array - Interessante reparar que ele manteve a pizza de chocolate, isto por que antes adicionamos a pizza de peito de peru
+pizzas.shift(); // Remove o primeiro elemento do Array - Assim como no exemplo anterior, ele manteve a pizza de 4 Queijos e removeu a pizza Gratinada, pois a adicionamos anteriormente
+
 
 pizzas.length; // Saber quantos elementos tem o Array
 
@@ -13,6 +16,26 @@ pizzas.length; // Saber quantos elementos tem o Array
 
 // Estrutura de repetição:
 
-for (let index = 0; index < pizzas.length; index++) {
-    console.log(pizzas[index]);
-}
+// for (let index = 0; index < pizzas.length; index++) {
+//     console.log(pizzas[index]);
+// }
+
+// Exercícios:
+// 1-
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu.indexOf("Serviços");
+console.log(menuServices);
+
+// 2-
+
+let menu2 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu2.indexOf("Portfólio");
+
+console.log(indexOfPortfolio);
+
+// 3-
+
+let menu3 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu3.push("Contato");
+console.log(menu3);
---
+++ fundamentos/bloco04JavascripteLogica/bloco04-02/for.js
@@ -0,0 +1,21 @@
+// Conteúdo vídeo
+
+let numero = 7;
+
+// for(index = 1; index <= 9; index += 1){
+//     console.log(numero * index);
+// }
+
+let listNames = ["Joana", "Lucas", "Maria", "João"];
+
+// for(index = 0; index < listNames.length; index += 1){
+//     console.log(listNames[index]);
+// }
+
+// Exercício:
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for(index = 0; index < groceryList.length; index += 1){
+    console.log(groceryList[index]);
+}
