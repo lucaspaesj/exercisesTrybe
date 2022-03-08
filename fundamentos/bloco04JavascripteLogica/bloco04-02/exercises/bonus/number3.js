16bd5e0f770743fbd6a7d0919b9650c4dc63c1b3
Lucas Paes Januzi <lpjanuzi@gmail.com>
2022-03-08 19:21:38 -0300

    Adicionando exercícios bonus

---
+++ fundamentos/bloco04JavascripteLogica/bloco04-02/exercises/bonus/number3.js
@@ -0,0 +1,16 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// 3-
+
+let multipliedNumbers = [];
+
+for(let index = 0; index < numbers.length; index += 1){
+    if(typeof numbers[index + 1] == "number"){
+        multipliedNumbers.push(numbers[index] * numbers[index + 1]);
+    }
+    else if(typeof numbers[index + 1] != "number"){
+        multipliedNumbers.push(numbers[index]);
+    }
+}
+
+console.log(multipliedNumbers);
