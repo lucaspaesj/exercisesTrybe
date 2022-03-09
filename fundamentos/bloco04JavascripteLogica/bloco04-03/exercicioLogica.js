41e5921e6f6cb396f5edc7adad423d5434abf249
Lucas Paes Januzi <lpjanuzi@gmail.com>
2022-03-09 15:31:39 -0300

    Incrementando lógica de programação e construindo algoritmos nos exercícios

--- fundamentos/bloco04JavascripteLogica/bloco04-03/exercicioLogica.js
+++ fundamentos/bloco04JavascripteLogica/bloco04-03/exercicioLogica.js
@@ -1,2 +1,13 @@
 let fruits = [3, 4, 10, 1, 12];
 
+let valorTotal = 0;
+for(let index = 0; index < fruits.length; index += 1){
+    valorTotal += fruits[index]
+}
+
+if(valorTotal > 15){
+    console.log(valorTotal);
+}
+else {
+    console.log("Valor menor que 16");
+}
