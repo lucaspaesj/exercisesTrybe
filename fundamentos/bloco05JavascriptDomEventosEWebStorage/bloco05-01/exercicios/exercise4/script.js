4ac9e82084f3fbe99e5dec7d3706af062ae97e36
Lucas Paes Januzi <lpjanuzi@gmail.com>
2022-03-14 15:47:56 -0300

    Adicionando exercício 4

---
+++ fundamentos/bloco05JavascriptDomEventosEWebStorage/bloco05-01/exercicios/exercise4/script.js
@@ -0,0 +1,10 @@
+// document.getElementByTagName - Retornados em formato de lista de elementos.
+// Função para acessar os elementos da nossa página
+// Pega todas as tags referidas e altera de acordo com o que quisermos
+
+console.log(document.getElementsByTagName("p"));
+
+console.log(document.getElementsByTagName("div"));
+
+document.getElementsByTagName("span")[0].innerText = "Carlos Sainz";
+
