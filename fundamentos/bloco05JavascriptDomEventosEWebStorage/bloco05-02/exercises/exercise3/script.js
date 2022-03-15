5dadd2ac29dfaf80082ba9d48dd5ed3fbf2b461e
Lucas Paes Januzi <lpjanuzi@gmail.com>
2022-03-15 15:26:34 -0300

    Adicionando exercício 3

---
+++ fundamentos/bloco05JavascriptDomEventosEWebStorage/bloco05-02/exercises/exercise3/script.js
@@ -0,0 +1,21 @@
+// Criando e adicionando elementos para o HTML pelo JS
+// 1- Crie um irmão para elementoOndeVoceEsta .
+function createElementChild(id, nameElement, text){
+    let pai = document.querySelector(id);
+    let brother = document.createElement(nameElement);
+    brother.id = text;
+    
+    pai.appendChild(brother);
+}
+document.querySelector("#pai").lastElementChild.id = "quartoFilho";
+createElementChild("#pai", "section", "quintoEUltimoFilho");
+// 2- Crie um filho para elementoOndeVoceEsta .
+document.querySelector("#elementoOndeVoceEsta").lastElementChild.id = "segundoFilhoDoFilho";
+createElementChild("#elementoOndeVoceEsta", "section", "terceiroEUltimoFilhoDoFilho");
+// 3- Crie um filho para primeiroFilhoDoFilho .
+let primeiroFilhoDoFilho = document.querySelector("#primeiroFilhoDoFilho");
+let filhoDoFilhoDoFilho = document.createElement("section");
+primeiroFilhoDoFilho.appendChild(filhoDoFilhoDoFilho);
+filhoDoFilhoDoFilho.id = "primeiroFilhoDoFilhoDoFilho";
+// 4- A partir desse filho criado, acesse terceiroFilho .
+console.log(document.querySelector("#primeiroFilhoDoFilhoDoFilho").parentElement.parentElement.nextElementSibling);
