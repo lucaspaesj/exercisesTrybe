41b85803818417d24760acfcb200daa08b6235e6
Lucas Paes Januzi <lpjanuzi@gmail.com>
2022-03-16 23:50:57 -0300

    Adicionando exercício 7

--- fundamentos/bloco05JavascriptDomEventosEWebStorage/bloco05-03/pratica/script.js
+++ fundamentos/bloco05JavascriptDomEventosEWebStorage/bloco05-03/pratica/script.js
@@ -162,3 +162,19 @@ Exercício 7:
 Implemente uma função que adiciona uma tarefa personalizada ao calendário. A função deve receber como parâmetro a string com o nome da tarefa (ex: "cozinhar") e criar dinamicamente um elemento com a tag <span> contendo a tarefa.
 O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
 */
+
+function addTask(str){
+    let divParent = document.querySelector(".my-tasks");
+    let newTask = document.createElement("span");
+    newTask.innerHTML = str + "<br>";
+    divParent.appendChild(newTask);
+}
+
+addTask("Aprender a programar em JS");
+addTask("Aprender a programar em Python");
+addTask("Formar");
+
+/*
+Exercício 8:
+
+*/
