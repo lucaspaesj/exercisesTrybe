c9fa2a11e5b355ef4fdfca0cb65406c3d7844acc
Lucas Paes Januzi <lpjanuzi@gmail.com>
2022-04-05 15:36:43 -0300

    Adicionando bloco07-03 e conteúdos

---
+++ fundamentos/bloco07JavaScriptES6ETestesUnitarios/bloco07-03/exercises/exercise1/newProject/tests.js
@@ -0,0 +1,8 @@
+// O módulo Assert é uma ferramenta que testa expressões. Esse módulo já vem instalado com o NodeJS . Podemos entender o assert como sendo uma expressão booleana que será sempre true , a menos que haja uma falha.
+// Abaixo temos um exemplo de como podemos fazer um teste simples com assert :
+
+// const assert = require('assert'); // Sintaxe para incluir o módulo assert
+
+// assert.strictEqual(50, 50); // Sem erros: 50 === 50
+// assert.strictEqual(50, 70); // AssertionError: 50 !== 70
+
