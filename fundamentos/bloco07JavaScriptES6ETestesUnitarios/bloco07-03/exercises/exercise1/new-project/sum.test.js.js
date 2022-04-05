3ab142ab5947a8a5e0fbac30caef0ec35fc14793
Lucas Paes Januzi <lpjanuzi@gmail.com>
2022-04-05 15:55:43 -0300

    modificando estrutura da pasta

--- fundamentos/bloco07JavaScriptES6ETestesUnitarios/bloco07-03/exercises/exercise1/new-project/sum.test.js.js
+++ fundamentos/bloco07JavaScriptES6ETestesUnitarios/bloco07-03/exercises/exercise1/new-project/sum.test.js.js
@@ -6,3 +6,8 @@
 // assert.strictEqual(50, 50); // Sem erros: 50 === 50
 // assert.strictEqual(50, 70); // AssertionError: 50 !== 70
 
+const sum = (a, b) => a + b;
+
+test('sums two values', () => {
+  expect(sum(2, 3)).toEqual(5);
+});
