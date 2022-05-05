8a4974210dc4f0c19b825071911e144f1360fa6c
lucaspaesj <lpjanuzi@gmail.com>
2022-05-05 16:31:35 -0300

    adicionando primeiros conteúdos do dia

---
+++ front-end/bloco01IntroducaoAReact/bloco02-01/my_app_2/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Paragraph from './Component';
+
+function App() {
+  return <Paragraph />
+}
+
+export default App;
---
+++ front-end/bloco01IntroducaoAReact/bloco02-01/my_app_2/src/Component.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Paragraph extends Component {
+    render () {
+        return (
+            <div>
+            <h1>Meu nome é Lucas Paes Januzi</h1>
+            <p>
+                Tenho 22 anos, sou residente em Belo Horizonte/MG. Tenho diversos interesses no desenvolvimento WEB Full-Stack e este é o meu primeiro componente React de classe!! VQV!
+            </p>
+            </div>
+        )
+    }
+}
+
+export default Paragraph;
