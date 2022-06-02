0d394f57bfc63cb12944b0e123cd6b6b576d3839
lucaspaesj <lpjanuzi@gmail.com>
2022-06-02 16:00:44 -0300

    adicionando exercício2

--- front-end/bloco14TestesRTL/bloco14-02/exercise1/src/App.test.js
+++ front-end/bloco14TestesRTL/bloco14-02/exercise1/src/App.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+afterEach(() => jest.clearAllMocks());
+
 describe('Testa página de piadas advidas de uma API', () => {
   it('Verifica que, quando recebo uma piada, ela é renderizada na tela', async () => {
     // global.fetch = (url) => {
@@ -23,4 +25,27 @@ describe('Testa página de piadas advidas de uma API', () => {
     const requiredJoke = await screen.findByText('Whiteboards ... are remarkable.');
     expect(requiredJoke).toBeInTheDocument();
   });
+
+  it('Fetches a joke', async () => {
+    const joke = {
+      id: '7h3oGtrOfxc',
+      joke: 'Whiteboards ... are remarkable.',
+      status: 200,
+    };
+  
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(joke),
+    });
+  
+    render(<App />);
+    const renderedJoke = await screen.findByText('Whiteboards ... are remarkable.');
+    expect(renderedJoke).toBeInTheDocument();
+    expect(global.fetch).toBeCalledTimes(1);
+    expect(global.fetch).toBeCalledWith(
+      'https://icanhazdadjoke.com/',
+      { headers: { Accept: 'application/json' } },
+    );
+  });
 });
+
